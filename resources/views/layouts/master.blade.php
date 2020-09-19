c7b7e7395a83fcdd75ae473529d2bea48ee90fd7
NaweedMM <naweedmdmahbub@gmail.com>
2020-09-19 15:31:09 +0600

    Fetched Admin LTE 3.0 template & sliced the index file into layouts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+@include('layouts.header')
+@include('layouts.scripts')
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <!-- Navbar -->
+@include('layouts.navbar')
+<!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+@include('layouts.sidebar')
+
+<!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        @yield('content')
+    </div>
+    <!-- /.content-wrapper -->
+@include('layouts.footer')
+
+<!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+</body>
+</html>
